0e0214f9d4a5d565d0dbab380b0fca4ea109dd88
Joshua7792 <j.santiago39@hotmail.com>
2024-01-03 13:12:01 -0400

    fixed

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,7 +1,7 @@
-#!/usr/bin/ env python3 
+#!/usr/bin/env python3
 """Type-annotated function to_str"""
 
 
 def to_str(n: float) -> str:
-    """Return float in str representation"""
+    """Return str representation of float"""
     return str(n)
