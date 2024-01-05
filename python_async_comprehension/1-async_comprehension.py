5083c1c83e5387658e6842692e6fd7f96bf0afc6
Joshua7792 <j.santiago39@hotmail.com>
2024-01-05 12:32:52 -0400

    fixed for checkers

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Coroutine with async comprehension """
-
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehension."""
     return [i async for i in async_generator()]
