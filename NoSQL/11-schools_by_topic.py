755120cc00f9503db5264287a42edb4c29566cd3
Joshua7792 <j.santiago39@hotmail.com>
2024-01-12 14:34:34 -0400

    serach by topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""that returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Return the list of schools having a specific topic."""
+    return list(mongo_collection.find({"topics": topic}))
