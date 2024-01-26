593424bc5b50fea8ea9a252615a38ab46ed8b5be
Joshua7792 <j.santiago39@hotmail.com>
2024-01-26 13:17:17 -0400

    fix the use of variables

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 export default function getResponseFromAPI () {
   // Returns a new Promise
   return new Promise((resolve, reject) => {
-    // The Promise should be able to resolve to this
-    resolve();
-    // The Promise should be able to reject to this
-    reject();
+    setTimeout(() => {
+      resolve('true');
+      reject(new Error('An error occurred'));
+    });
   });
 }
