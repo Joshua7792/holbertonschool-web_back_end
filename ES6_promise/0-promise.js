9a4d452cc7d350ba07a8a3696df1573fa2020378
Joshua7792 <j.santiago39@hotmail.com>
2024-01-26 11:59:21 -0400

    Return a Promise

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+// 0-promise.js
+export default function getResponseFromAPI() {
+  // Returns a new Promise
+  return new Promise((resolve, reject) => {
+      // Promise logic goes here. For this task, it can be empty as
+      // there are no specific requirements for what the Promise should do.
+  });
+}
