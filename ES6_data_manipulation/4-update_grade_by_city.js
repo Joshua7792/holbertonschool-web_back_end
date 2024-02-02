f779d01a7798953fe95ebe67c0360c8ffd00a1c7
Joshua7792 <j.santiago39@hotmail.com>
2024-02-02 11:41:55 -0400

    fixed

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   // Use the filter function to get students in the specified city
   // Then, use the map function to modify each student object
-  return students.filter(student => student.location === city)
+  return students.filter((student) => student.location === city)
     .map(student => {
       // Find the new grade for the student, if it exists
-      const gradeObject = newGrades.find(grade => grade.studentId === student.id);
+      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
       // Return a new object for the student, adding a grade property
       // If the student does not have a new grade, set grade to 'N/A'
       return {
