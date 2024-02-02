c9f50d50196f2a5e5dd13036ae89d3d195522686
Joshua7792 <j.santiago39@hotmail.com>
2024-02-02 13:03:57 -0400

    has values

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 export default function hasArrayValues(set, array) {
   // Return true if all the values in the array are present in the set
   // Return false if not
-  return array.every(value => set.has(value));
+  return array.every((value) => set.has(value));
 }
