b388006be76dd009acd862fd665c055199adff92
Joshua7792 <j.santiago39@hotmail.com>
2024-02-02 12:59:01 -0400

    set arrays

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,5 @@
+export default function setFromArray(array) {
+  // Return a new Set constructed from the provided array
+  // Sets automatically remove duplicate values
+  return new Set(array);
+}
